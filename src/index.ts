adcd281d10d2dd158494f5dabdef9f64b3d9c5ce
Lache Melvin <lache.melvin@gmail.com>
2021-10-29 10:56:06 +1300

    add initial graphql test message

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log('Hello world!')
+import { ApolloServer } from 'apollo-server'
+
+import resolvers from './resolvers'
+import typeDefs from './typeDefs'
+
+const server = new ApolloServer({ resolvers, typeDefs })
+
+server.listen()
+  .then(({ url }) => console.log(`Server is ready at ${url}`))
+
