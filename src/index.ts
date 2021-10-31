7640a865879816032700b48af6e4a7503aa7985a
Lache Melvin <lache.melvin@gmail.com>
2021-11-01 11:31:20 +1300

    add typeorm for DB mapping

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,32 @@
+import "reflect-metadata";
 import { ApolloServer } from "apollo-server";
+import { createConnection } from "typeorm";
+import { Frog } from "./entity/Frog";
 
 import resolvers from "./resolvers";
 import typeDefs from "./typeDefs";
 
-const server = new ApolloServer({ resolvers, typeDefs });
+(async () => {
+  try {
+    const connection = await createConnection();
+    let frogRepository = connection.getRepository(Frog);
 
-server.listen().then(({ url }) => console.log(`Server is ready at ${url}`));
+    // --- If u got no frogs ---
+    // console.log("Inserting a new frog into the database...");
+    // const frog = new Frog();
+    // frog.name = "Filip";
+    // frog.breed = "Tree Frog";
+    // await frogRepository.save(frog);
+    // console.log("saved");
+
+    console.log("Loading frogs from the database...");
+    const frogs = await frogRepository.find();
+    console.log("Loaded frogs: ", frogs);
+
+    const server = new ApolloServer({ resolvers, typeDefs });
+
+    server.listen().then(({ url }) => console.log(`Server is ready at ${url}`));
+  } catch (error) {
+    console.error(error);
+  }
+})();
